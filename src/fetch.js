5e9b757af669d8db2d04f2c8b6735b358fa95d75
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-03-27 00:38:28 +0500

    refactored scripts and added linear gradient background

--- src/fetch.js
+++ src/fetch.js
@@ -1,152 +1,157 @@
-global.fetch = fetch;
-
-const weather = {};
-let bckgImg = '';
-let city = '';
-
 const FetchImage = (() => {
-  const showImage = () => {
-    const container = document.querySelector('.container');
-    const img = document.createElement('img');
-    img.src = bckgImg;
-    img.classList.add('bckg-img');
-    container.appendChild(img);
-  };
-  const getImage = (desc, name) => {
-    const apiID = 'NLl2CVUZVGeBng2hZtpIOTO1nuGKTaPTvFORb6sEyOw';
-    const url = `https://api.unsplash.com/search/photos/?client_id=${apiID}&query=${desc}-${name}`;
-
-    fetch(url)
-      .then((response) => response.json())
-      .then((response) => {
-        const imgs = response.results;
-        const random = Math.floor((Math.random() * (imgs.length - 1)) + 0);
-        bckgImg = `${response.results[`${random}`].urls.regular}`;
-      })
-      .then(() => showImage());
+  const getImage = async (desc, name) => {
+    try {
+      const apiID = 'NLl2CVUZVGeBng2hZtpIOTO1nuGKTaPTvFORb6sEyOw';
+      const url = `https://api.unsplash.com/search/photos/?client_id=${apiID}&query=${desc}-${name}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      return err;
+    }
   };
   return { getImage };
 })();
 
 const FetchWeather = (() => {
   const appID = 'd5760da932f26c14aa01b5addbc6ab5f';
-
-  const cToF = (temp) => (Math.floor((9 / 5) * temp) + 32);
-
-  const handler = () => {
-    const tmp = Math.floor(weather.temp - 270);
-    const temp = document.querySelector('.temp');
-    temp.addEventListener('click', () => {
-      if (weather.stat === 0) {
-        temp.innerHTML = '';
-        // const f = cToF(tmp);
-        temp.innerHTML = `${cToF(tmp)}&#176 F`;
-        weather.stat = 1;
-      } else {
-        temp.innerHTML = '';
-        temp.innerHTML = `${tmp}&#176 C`;
-        weather.stat = 0;
-      }
-    });
-  };
-
-  const ui = () => {
-    const name = document.querySelector('.name');
-    name.innerText = `${weather.name}, ${weather.cntr}`;
-
-    const temp = document.querySelector('.temp');
-    temp.innerHTML = `${Math.floor(weather.temp - 270)}&#176 C`;
-    handler();
-
-    const time = document.querySelector('.time');
-    const currentTimeDate = new Date();
-    let minutes = currentTimeDate.getMinutes();
-    minutes = minutes < 10 ? `0${minutes}` : minutes;
-    time.innerText = `${currentTimeDate.getHours()}:${minutes}`;
-
-    const days = [
-      'Sun',
-      'Mon',
-      'Tue',
-      'Wed',
-      'Thu',
-      'Fri',
-      'Sat',
-    ];
-    const months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ];
-    const today = document.querySelector('.date');
-    const day = days[currentTimeDate.getDay()];
-    const month = months[currentTimeDate.getMonth()];
-    const date = currentTimeDate.getDate();
-    const year = currentTimeDate.getFullYear();
-    today.innerText = `${day}, ${month} ${date}, ${year}`;
-
-    const desc = document.querySelector('.desc');
-    desc.innerText = weather.description;
-
-    const icon = document.querySelector('.icon');
-    icon.src = `http://openweathermap.org/img/w/${weather.icon}.png`;
-
-    const windSpeed = document.querySelector('.wind-speed');
-    windSpeed.innerText = `${weather.windSpeed} m/h`;
-
-    const visibility = document.querySelector('.visibility');
-    visibility.innerText = `${((weather.visibility) / 1000).toFixed(2)} km`;
-
-    const humidity = document.querySelector('.humidity');
-    humidity.innerText = `${weather.humidity}%`;
-
-    const clouds = document.querySelector('.clouds');
-    clouds.innerText = `${weather.clouds}%`;
-  };
+  const init = (data) => ({
+    name: data.name,
+    cntr: data.sys.country,
+    temp: data.main.temp,
+    stat: 0,
+    description: data.weather[0].description,
+    icon: data.weather[0].icon,
+    windSpeed: data.wind.speed,
+    visibility: data.visibility,
+    humidity: data.main.humidity,
+    clouds: data.clouds.all,
+  });
 
   const getWeather = async (location) => {
-    await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${appID}`, { mode: 'cors' })
-      .then((response) => response.json())
-      .then((response) => {
-        weather.name = response.name;
-        weather.cntr = response.sys.country;
-        weather.temp = response.main.temp;
-        weather.stat = 0;
-        weather.description = response.weather[0].description;
-        weather.icon = response.weather[0].icon;
-        weather.windSpeed = response.wind.speed;
-        weather.visibility = response.visibility;
-        weather.humidity = response.main.humidity;
-        weather.clouds = response.clouds.all;
-        FetchImage.getImage(weather.description, weather.name);
-      })
-      .then(() => {
-        ui();
-      });
+    try {
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${appID}`;
+      const response = await fetch(url, { mode: 'cors' });
+      const data = await response.json();
+      return init(data);
+    } catch (err) {
+      return err;
+    }
   };
 
-  return { getWeather, cToF };
+  return { getWeather };
 })();
 
 const Geolocation = (() => {
-  const getLoc = () => {
-    fetch('https://ipapi.co/json/')
-      .then((response) => response.json())
-      .then((response) => {
-        city = `${response.city}`;
-        FetchWeather.getWeather(city);
-      });
+  const getLoc = async () => {
+    try {
+      const response = await fetch('https://ipapi.co/json/');
+      const data = await response.json();
+      return `${data.city}`;
+    } catch (err) {
+      return err;
+    }
   };
   return { getLoc };
 })();
 
-export { Geolocation, FetchWeather };
+const UI = (() => {
+  const container = document.querySelector('.container');
+  const img = document.createElement('img');
+  const name = document.querySelector('.name');
+  const temp = document.querySelector('.temp');
+
+  const time = document.querySelector('.time');
+  const currentTimeDate = new Date();
+  let minutes = currentTimeDate.getMinutes();
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  const days = [
+    'Sun',
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat',
+  ];
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const today = document.querySelector('.date');
+  const day = days[currentTimeDate.getDay()];
+  const month = months[currentTimeDate.getMonth()];
+  const date = currentTimeDate.getDate();
+  const year = currentTimeDate.getFullYear();
+  const desc = document.querySelector('.desc');
+  const icon = document.querySelector('.icon');
+  const windSpeed = document.querySelector('.wind-speed');
+  const visibility = document.querySelector('.visibility');
+  const humidity = document.querySelector('.humidity');
+  const clouds = document.querySelector('.clouds');
+
+  const cToF = (temp) => (Math.floor((9 / 5) * temp) + 32);
+
+  const render = (data) => {
+    name.innerText = `${data.name}, ${data.cntr}`;
+    temp.innerHTML = `${Math.floor(data.temp - 270)}&#176 C`;
+    temp.addEventListener('click', () => {
+      const tmp = Math.floor(data.temp - 270);
+      if (data.stat === 0) {
+        temp.innerHTML = '';
+        temp.innerHTML = `${cToF(tmp)}&#176 F`;
+        data.stat = 1;
+      } else {
+        temp.innerHTML = '';
+        temp.innerHTML = `${tmp}&#176 C`;
+        data.stat = 0;
+      }
+    });
+
+    time.innerText = `${currentTimeDate.getHours()}:${minutes}`;
+    today.innerText = `${day}, ${month} ${date}, ${year}`;
+
+    desc.innerText = `${data.description}`;
+    icon.src = `http://openweathermap.org/img/w/${data.icon}.png`;
+    windSpeed.innerText = `${data.windSpeed} m/h`;
+    visibility.innerText = `${((data.visibility) / 1000).toFixed(2)} km`;
+    humidity.innerText = `${data.humidity}%`;
+    clouds.innerText = `${data.clouds}%`;
+  };
+  const currentLocWeather = async () => {
+    const currentLoc = await Geolocation.getLoc();
+    const response = await FetchWeather.getWeather(`${currentLoc}`);
+    const bckgImg = await FetchImage.getImage(response.description, response.name);
+    const imgs = await bckgImg.results;
+    const random = Math.floor((Math.random() * (imgs.length - 1)) + 0);
+    const ran = `${bckgImg.results[`${random}`].urls.regular}`;
+    img.src = ran;
+    img.classList.add('bckg-img');
+    container.appendChild(img);
+    render(response);
+  };
+  const search = async (city) => {
+    const weatherData = await FetchWeather.getWeather(city);
+    const bckgImg = await FetchImage.getImage(weatherData.description, weatherData.name);
+    const imgs = await bckgImg.results;
+    const random = Math.floor((Math.random() * (imgs.length - 1)) + 0);
+    const ran = `${bckgImg.results[`${random}`].urls.regular}`;
+    img.src = ran;
+    img.classList.add('bckg-img');
+    container.appendChild(img);
+    render(weatherData);
+  };
+  return { currentLocWeather, search };
+})();
+
+export { Geolocation, FetchWeather, UI };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import _ from 'lodash';
 import './assets/style.css';
 
-import { FetchWeather, Geolocation } from './fetch';
+import { UI } from './fetch';
 
-Geolocation.getLoc();
+UI.currentLocWeather();
 const eventListener = () => {
   const btn = document.querySelector('#searchBtn');
-
   const city = document.querySelector('#city-val');
   btn.addEventListener('click', () => {
-    FetchWeather.getWeather(city.value);
+    UI.search(city.value);
   });
-
-  // const temp = document.querySelector('.temp');
-  // temp.addEventListener('click', () => {
-
-  // });
 };
 
 eventListener();
